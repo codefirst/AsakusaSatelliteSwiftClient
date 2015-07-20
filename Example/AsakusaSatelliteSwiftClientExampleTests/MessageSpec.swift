50da03357ec755012fb17c172fbd9d2fdcb02329
banjun <banjun@gmail.com>
2015-07-20 12:30:46 +0900

    test for Message serialize and deserialize

--- Example/AsakusaSatelliteSwiftClientExampleTests/MessageSpec.swift
+++ Example/AsakusaSatelliteSwiftClientExampleTests/MessageSpec.swift
@@ -8,13 +8,19 @@
 
 import AsakusaSatellite
 import SwiftyJSON
+import Foundation
 import Quick
 import Nimble
 
 
-private func testJSON(basename: String) -> JSON {
-    let path = NSBundle(forClass: MessageSpec.self).pathForResource(basename, ofType: "json")!
-    return JSON(data: NSData(contentsOfFile: path)!, options: .AllowFragments, error: nil)
+private func testDataPath(subpath: String) -> String {
+    return NSBundle(forClass: MessageSpec.self).pathForResource(subpath.lastPathComponent.stringByDeletingPathExtension,
+        ofType: subpath.pathExtension,
+        inDirectory: subpath.stringByDeletingLastPathComponent)!
+}
+
+private func testJSON(subpath: String) -> JSON {
+    return JSON(data: NSData(contentsOfFile: testDataPath(subpath))!, options: [], error: nil)
 }
 
 
@@ -22,9 +28,35 @@ class MessageSpec: QuickSpec {
     override func spec() {
         describe ("load from json") {
             it ("load from message.json") {
-                let m = Message(json: testJSON("message"))
-                expect(m).notTo(beNil())
-                expect(m!.body).to(equal("example message"))
+                guard let m = Message(json: testJSON("message.json")) else { return fail() }
+                expect(m.body).to(equal("example message"))
+            }
+        }
+        
+        describe ("serialize") {
+            it ("serialize to file and deserialize from it") {
+                guard let m1 = Message(json: JSON([
+                    "id": "id",
+                    "name": "name",
+                    "screen_name": "screen_name",
+                    "body": "body",
+                    "html_body": "html_body",
+                    "profile_image_url": "http://example.com/example.png",
+                    "created_at": "2015-12-31 23:59:59+0900",
+                    ])) else { return fail() }
+                
+                let file = NSTemporaryDirectory().stringByAppendingPathComponent("serialized.json")
+                do {try NSFileManager.defaultManager().removeItemAtPath(file)} catch _ {}
+                m1.saveToFile(file)
+                
+                guard let m2 = Message(file: file) else { return fail() }
+                expect(m2.id).to(equal(m1.id))
+                expect(m2.name).to(equal(m1.name))
+                expect(m2.screenName).to(equal(m1.screenName))
+                expect(m2.body).to(equal(m1.body))
+                expect(m2.htmlBody).to(equal(m1.htmlBody))
+                expect(m2.profileImageURL).to(equal(m1.profileImageURL))
+                expect(m2.createdAt).to(equal(m1.createdAt))
             }
         }
     }
